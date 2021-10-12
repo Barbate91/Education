3293540e09ab4bc0b4b705eecdb9b0dbe7c247aa
jake.armstrong <armstrong.jester@gmail.com>
2021-10-11 23:43:25 -0700

    leetcode examples

---
+++ src/main/leetcode/Solution1929.java
@@ -0,0 +1,11 @@
+package main.leetcode;
+
+public class Solution1929 {
+    public int[] getConcatenation(int[] nums) {
+        int arrLength = nums.length;
+        int[] result = new int[nums.length * 2];
+        System.arraycopy(nums, 0, result, 0, arrLength);
+        System.arraycopy(nums, 0, result, arrLength, arrLength);
+        return result;
+    }
+}
---
+++ src/main/leetcode/Solution2011.java
@@ -0,0 +1,17 @@
+package main.leetcode;
+
+public class Solution2011 {
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        char target = '+';
+        for (int i=0; i<operations.length; i++) {
+            String operation = operations[i];
+            if (operation.indexOf(target) > -1) {
+                result++;
+            } else {
+                result--;
+            }
+        }
+        return result;
+    }
+}
